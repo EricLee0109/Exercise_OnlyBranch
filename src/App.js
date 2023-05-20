e6a0e7af525e1d113761b18f011ec872bc55e1af
Eric Lee <lethaikhoa0109@gmail.com>
2023-05-20 10:17:45 +0700

    Components Part 2

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Navigation from './Components/Navigation';
+import Player from './Components/Player';
+
+function App() {
+  return (
+    <div className="App">
+      <Navigation/>
+      <Player/>
+    </div>
+  );
+}
+
+export default App;
